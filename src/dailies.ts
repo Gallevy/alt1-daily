90ac78b97c565955857ddf496fea65081b900d3d
Gal <gallevy3313@gmail.com>
2022-02-07 15:44:33 +0200

    aaa

---
+++ src/dailies.ts
@@ -0,0 +1,19 @@
+const dailies = [
+  "Rock Prif (5m)",
+  "Rocks Oglog (5m)",
+  "Farming (temporary until 120)",
+  "Farm Tree route",
+  "is wax (5m)",
+  "Crystal tree",
+  "Ports (5m morning, 5m night)",
+  "Sinkholes",
+  "Tokkul",
+  "x The Arc",
+  "Motherload Maw",
+  "50 daily bosses dominion tower",
+  "Daily challenge [DG]",
+  "Daily challenge [Slayer]",
+  "Daily challenge [Herblore]",
+];
+
+export default dailies;
